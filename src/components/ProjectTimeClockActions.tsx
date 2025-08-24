d78c9b546ded8f2bdecd617fafc42d30cc489089
Arvydas Vingis <arvydasvinys@gmail.com>
2025-08-24 14:21:27 +0300

    Add clock button disabling and according styles

--- src/components/ProjectTimeClockActions.tsx
+++ src/components/ProjectTimeClockActions.tsx
@@ -4,23 +4,27 @@ import styles from "../styles/projectTimeClock.module.css";
 type ProjectTimeClockActionsProps = {
 	handleStart: () => void;
 	handleStop: () => void;
+	isOngoing: boolean;
 };
 
 function ProjectTimeClockActions({
 	handleStart,
 	handleStop,
+	isOngoing,
 }: ProjectTimeClockActionsProps) {
 	return (
 		<div>
 			<button
 				className={clsx("btn", styles.clockButton)}
 				onClick={handleStart}
+				disabled={isOngoing}
 			>
 				Start
 			</button>
 			<button
 				className={clsx("btn-alt", styles.clockButton)}
 				onClick={handleStop}
+				disabled={!isOngoing}
 			>
 				Stop
 			</button>
